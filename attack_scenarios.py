c51646ddfbb942b785c196724a2113f1ed97ef80
killian-bertsch <killian.bertsch@gmail.com>
2025-04-24 18:13:40 -0600

    Test

--- attack_scenarios.py
+++ attack_scenarios.py
@@ -1,8 +1,28 @@
+import subprocess
+
+# 
+# 
 # TODO: Define create_file_access_scenario() with sandboxed and unsandboxed processes
+
+# Simulates unauthorized access to a file
+#
+#
+def fileAcessScenario():
+    with open("/etc/shadow", "r") as f: # permission denied as of right now
+        data = f.read()
+        print(data)
 
 # TODO: Define create_network_access_scenario()
 
+def networkAcessScenario():
+    subprocess.run(["curl", "http://example.com", "-o", "/tmp/network_test.html"])
+
 # TODO: Define create_privilege_escalation_scenario()
+
+def privelegeEscelationScenario():
+    subprocess.run(["sudo", "touch", "/root/hacked.txt"], stderr=subprocess.PIPE)
 
 # TODO: Return list of processes for each scenario
 
+if __name__ == "__main__":
+    fileAcessScenario()
